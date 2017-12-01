c9350eb150ec0dad61b16b29506945b525ca3f42
irusvind <irus@live.dk>
2017-12-01 10:34:43 +0100

    Spawn

---
+++ 1.SemesterProject/src/UI/GUI/SpawnNPCController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI.GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author irus
+ */
+public class SpawnNPCController implements Initializable {
+
+    gridpane.add(new imageview)
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
